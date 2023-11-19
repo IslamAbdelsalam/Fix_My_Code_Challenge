552819f214bdf8b8a6a80181271415aa54f7b5a6
IslamAbdelslam <islamabdoulradi@gmail.com>
2023-11-19 11:58:17 +0200

    👌

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -28,18 +28,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 1; i < index && current != NULL; i++)
+	for (i = 0; current != NULL && i < index - 1; i++)
 	{
 		current = current->next;
 	}
 
-	if (current == NULL)
+	if (current == NULL || current->next == NULL)
 		return (-1);
 
+	current->next->prev = current->prev;
 	current->prev->next = current->next;
-
-	if (current->next != NULL)
-		current->next->prev = current->prev;
 
 	free(current);
 
