89f06c1a3c6d11b36a67103bb915aca02e5aa74a
IslamAbdelslam <islamabdoulradi@gmail.com>
2023-11-19 11:45:31 +0200

    fix index

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -28,7 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 0; i < index && current != NULL; i++)
+	for (i = 1; i < index && current != NULL; i++)
 	{
 		current = current->next;
 	}
